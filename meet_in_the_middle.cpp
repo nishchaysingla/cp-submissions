7663288ef715bf899fb8d2721e67383e1fde324c
nishchaysingla <nishchaysingla1234@gmail.com>
2025-05-25 15:54:45 +0530

    Meet in the middle

---
+++ meet_in_the_middle.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define endl '\n'
+#define forn(i,n) for(int i = 0; i < n; i++)
+#define Code ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define all(vec) vec.begin(),vec.end()
+#define printy cout<<"YES"<<endl
+#define printn cout<<"NO"<<endl
+using namespace std;
+long double PI = acos(-1.0);
+int M = 1e9 + 7;
+
+#ifdef DEBUG
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+#else
+#define debarr(a,n)
+#define debmat(mat,row,col)
+#define pr(...)
+#endif
+
+int bin_expo(int a, int b){
+    int res = 1;
+    while(b){
+        if(b%2){
+            res = (res * a) % M;
+            b--;
+        }
+        else{
+            a = (a * a) % M;
+            b = b / 2;
+        }
+    }
+    return res;
+}
+
+int nCr(int n , int r){
+    int ans = 1;
+    for (int i = 0; i < r;i++){
+        ans *= (n - i);
+        ans /= (i + 1);
+    }
+    return ans;
+}
+
+// template <typename T>
+// T maxT(T a, T b){ return (a > b) ? a : b; }
+
+auto cmp = [](const pair<int, int> &a, const pair<int, int> &b){
+    if(a.first == b.first){
+        return a.second < b.second;
+    }
+    return a.first > b.first;
+    // priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)> pq(cmp);
+};
+void generate(int i , int n , vector<int>&nums, vector<int>&store,int sum){
+    
+    if(i==n){
+        store.push_back(sum);
+        return;
+    }
+    generate(i+1,n,nums,store,sum + nums[i]);
+    generate(i+1,n,nums,store,sum);
+    return;
+    
+}
+void solve(){
+    int n,x;cin>>n>>x;
+    vector<int>v(n);
+    forn(i,n){
+        cin>>v[i];
+    }
+    vector<int>v1,v2;
+    forn(i,n){
+        if(i%2){
+            v1.push_back(v[i]);
+        }
+        else{
+            v2.push_back(v[i]);
+        }
+    }
+    pr(v1);
+    pr(v2);
+    vector<int>gen1,gen2;
+    int n1 = v1.size();
+    int n2 = v2.size();
+    generate(0,n1,v1,gen1,0);
+    generate(0,n2,v2,gen2,0);
+    sort(gen2.begin(),gen2.end());
+    // sort(gen1.begin(),gen1.end());
+    int ans = 0;
+    pr(gen1);
+    pr(gen2);
+    for(auto &s : gen1){
+        auto it = upper_bound(gen2.begin(),gen2.end(),x-s);
+        // it--;
+        int nums = it - gen2.begin();
+        // cout << *it << " ";
+        ans += nums;
+        // if(*it == x-s){
+        //     // cout << s << " " << x - s << endl;
+        //     ans++;
+        // }
+    }
+    // cout << endl;
+    cout<<ans<<endl;
+}
+
+signed main(){
+    Code
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    int _t=1;
+    cin>>_t;
+    while(_t--){
+        solve();
+    }
+    return 0;
+}
