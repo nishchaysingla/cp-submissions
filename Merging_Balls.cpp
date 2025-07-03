79c8afdbda1e4e8c79e3a47e0ee56932425896f2
Nishchay Singla <nishchaysingla1234@gmail.com>
2025-07-03 11:44:03 +0530

    2 qns

---
+++ Merging_Balls.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define endl '\n'
+#define rep(i,a,b) for(int i = a; i < (b); ++i)
+#define rrep(i,a,b) for(int i = a; i > (b); --i)
+#define trav(x,a) for(auto& x:a)
+#define F first
+#define S second
+#define tk(n,v) forn(i,n){cin>>v[i];}
+#define Code ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define all(vec) vec.begin(),vec.end()
+#define yo cout<<"YES"<<endl
+#define noo cout<<"NO"<<endl
+using namespace std;
+long double PI = acos(-1.0);
+int M = 1e9 + 7;
+
+#ifdef DEBUG
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+#else
+#define debarr(a,n)
+#define debmat(mat,row,col)
+#define pr(...)
+#endif
+
+int bin_expo(int a, int b){
+    int res = 1;
+    while(b){
+        if(b%2){
+            res = (res * a) % M;
+            b--;
+        }
+        else{
+            a = (a * a) % M;
+            b = b / 2;
+        }
+    }
+    return res;
+}
+
+int nCr(int n , int r){
+    int ans = 1;
+    for (int i = 0; i < r;i++){
+        ans *= (n - i);
+        ans /= (i + 1);
+    }
+    return ans;
+}
+
+auto cmp = [](const pair<int, int> &a, const pair<int, int> &b){
+    if(a.first == b.first){
+        return a.second < b.second;
+    }
+    return a.first > b.first;
+};
+int n;
+int x[501];
+int dp[501][501];
+int recur(int l ,int r){
+    if(l==r){
+        return 0;
+    }
+    if(dp[l][r]!=-1)return dp[l][r];
+    int ans = 1e9;
+    for(int mid = l;mid<r;mid++){
+        ans = min(ans,recur(l,mid)+recur(mid+1,r)+((x[mid]-x[l-1]+100)%100 * (x[r]-x[mid]+100)%100 ));
+    }
+    return dp[l][r] = ans;
+}
+void solve(){
+    cin>>n;
+    memset(dp,-1,sizeof(dp));
+    x[0] = 0;
+    rep(i,1,n+1){
+        cin>>x[i];
+        x[i] %= 100;
+    }
+    rep(i,2,n+1){
+        x[i] += x[i-1];
+        x[i] %= 100;
+    }
+    cout<<recur(1,n)<<endl;
+}
+
+signed main(){
+    Code
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    int _t=1;
+    // cin>>_t;
+    while(_t--){
+        solve();
+    }
+    return 0;
+}
